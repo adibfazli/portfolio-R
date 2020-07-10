5a782543f348de20d362eaf998b24878495ba283
System Administrator <adib.fazlii@gmail.com>
2020-07-10 13:18:45 -0700

    Updates

---
+++ precache-manifest.90ab5333998e510d517727b70365c2ac.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669987de4757b6b8bd4e6e767c6092b1",
+    "url": "/portfolio-R/index.html"
+  },
+  {
+    "revision": "6a63136b7b8d2c6fc58c",
+    "url": "/portfolio-R/static/css/main.42b42468.chunk.css"
+  },
+  {
+    "revision": "68965243ec3c2ad31fb2",
+    "url": "/portfolio-R/static/js/2.67dc7c54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-R/static/js/2.67dc7c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a63136b7b8d2c6fc58c",
+    "url": "/portfolio-R/static/js/main.9d4f08e9.chunk.js"
+  },
+  {
+    "revision": "65d03b69b75eaa34226d",
+    "url": "/portfolio-R/static/js/runtime-main.737cbbe2.js"
+  },
+  {
+    "revision": "4bd153ab084dc35783e86a6ba82bef85",
+    "url": "/portfolio-R/static/media/blurCac.4bd153ab.png"
+  },
+  {
+    "revision": "aa51daa8e6c021198dccb74e3051925a",
+    "url": "/portfolio-R/static/media/cactus.aa51daa8.png"
+  },
+  {
+    "revision": "df55bbad4663a6599d729645f90f9d07",
+    "url": "/portfolio-R/static/media/cloud.df55bbad.png"
+  },
+  {
+    "revision": "5b5de989c60ef2b80ab239733b589281",
+    "url": "/portfolio-R/static/media/m.5b5de989.png"
+  },
+  {
+    "revision": "49aaf4c78a5e5be8e8584ca884704290",
+    "url": "/portfolio-R/static/media/me.49aaf4c7.gif"
+  },
+  {
+    "revision": "2672b97c516441f3c1199c6e9c4997ce",
+    "url": "/portfolio-R/static/media/speechBubble.2672b97c.png"
+  },
+  {
+    "revision": "bb035bf8d4e3b6ecd615d55e03e23fe4",
+    "url": "/portfolio-R/static/media/tumbleweed.bb035bf8.png"
+  }
+]);
